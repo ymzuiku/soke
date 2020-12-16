c699d84d339aeac74b06f272df748ca2053158bb
pillar <ymblender@gamil.com>
2020-12-16 17:39:12 +0800

    feat: update version

--- umd/index.d.ts
+++ umd/index.d.ts
@@ -2,6 +2,34 @@ import * as regExps from "./regExps";
 import { message } from "./message";
 import { Soke } from "./types";
 export { regExps, message };
+/**
+ * # 入口函数
+ *
+ *  ## 例子
+ * ```ts
+ * soke.baseLangaue = "zh"; // zh ｜ en, default: en
+ *
+ * const schema = soke({
+ *   name: {
+ *     minLength: 4,
+ *     maxLength: 12,
+ *     check: /Abc/,
+ *   },
+ *   age: {
+ *     minNum: 0,
+ *     maxNum: 100,
+ *     type: "number",
+ *   },
+ *   phone: {
+ *     cnIphone: 1,
+ *   },
+ *   user: {
+ *     message: (key, value) => `字段 ${key} 错误`,
+ *     account: [6, 20],
+ *   },
+ * });
+ * ```
+ */
 declare function soke<S extends Soke>(schema: S): (obj: any, lang?: "zh" | "en") => {
     [K in keyof S]: S[K] | any;
 };
